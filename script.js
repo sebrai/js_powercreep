abb01ea45623cfde8b80b74980f063aee6014367
sebrai <sebbranden@gmail.com>
2026-03-20 09:24:00 +0100

    removed background immage

--- script.js
+++ script.js
@@ -102,18 +102,18 @@ let game = {
     },
     set_start_btn: function (text) {
         ctx.clearRect(0, 0, size[0], size[1])
-        // ctx.fillStyle = "#000000"
-        // ctx.fillRect(0, 0, size[0], size[1])
-        let img = new Image(size[0],size[1])
-        img.crossOrigin = "anonymous"
-        img.src = "/img/background.jpg"
-        img.onload = ()=>{
-            img.width =size[0]
-            img.height = size[1]
-            ctx.drawImage(img,0,0,size[0],size[1])
-            draw_button(size[0] / 3, size[1] / 3, 300, 120, text, () => { game.start() }, true)
-        }
-
+        ctx.fillStyle = "#000000"
+        ctx.fillRect(0, 0, size[0], size[1])
+        // let img = new Image(size[0],size[1])
+        // img.crossOrigin = "anonymous"
+        // img.src = "/img/background.jpg"
+        // img.onload = ()=>{
+        //     img.width =size[0]
+        //     img.height = size[1]
+        //     ctx.drawImage(img,0,0,size[0],size[1])
+           
+        // }
+ draw_button(size[0] / 3, size[1] / 3, 300, 120, text, () => { game.start() }, true)
         
     },
     show_lives: function () {
